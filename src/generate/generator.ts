7bbe8be8b16e1b6be2a90b23be9c605b574581a5
akahoshi1421 <akaho1421@gmail.com>
2024-12-17 05:01:34 +0900

    add: Create

--- src/generate/generator.ts
+++ src/generate/generator.ts
@@ -1,5 +1,6 @@
 import { getGassmaAnyUse } from "./typeGenerate/gassmaAnyUse";
 import { getGassmaController } from "./typeGenerate/gassmaController";
+import { getGassmaCreate } from "./typeGenerate/gassmaCreate";
 import { getGassmaCreateMany } from "./typeGenerate/gassmaCreateMany";
 import { getGassmaMain } from "./typeGenerate/gassmaMain";
 import { getGassmaSheet } from "./typeGenerate/gassmaSheet";
@@ -11,6 +12,7 @@ const generater = (dictYaml: Record<string, Record<string, unknown[]>>) => {
   result += getGassmaSheet(sheetNames);
   result += getGassmaController(sheetNames);
   result += getGassmaAnyUse(dictYaml);
+  result += getGassmaCreate(sheetNames);
   result += getGassmaCreateMany(sheetNames);
 
   return result;
