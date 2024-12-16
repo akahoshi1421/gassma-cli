0be4f96fb399bee40ac77ec39e24babb4c7d28ca
akahoshi1421 <akaho1421@gmail.com>
2024-12-17 00:39:23 +0900

    add: mainTypeDeclare

---
+++ src/generate/typeGenerate/gassmaMain.ts
@@ -0,0 +1,15 @@
+const getGassmaMain = () => {
+  const mainTypeDeclare = `
+    export declare namespace Gassma {
+        class GassmaClient {
+            constructor(id?: string);
+
+            readonly sheets: GassmaSheet;
+        }
+    }
+    `;
+
+  return mainTypeDeclare;
+};
+
+export { getGassmaMain };
