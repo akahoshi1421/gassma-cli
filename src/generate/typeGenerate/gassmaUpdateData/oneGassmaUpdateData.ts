93ce708ae63329fc8a394f3112077c42bc339d89
akahoshi1421 <akaho1421@gmail.com>
2024-12-18 02:24:07 +0900

    fix: updateMany => update

--- src/generate/typeGenerate/gassmaUpdateData/oneGassmaUpdateData.ts
+++ src/generate/typeGenerate/gassmaUpdateData/oneGassmaUpdateData.ts
@@ -1,6 +1,6 @@
 const getOneGassmaUpdateData = (sheetName: string) => {
   return `
-export type Gassma${sheetName}UpdateManyData = {
+export type Gassma${sheetName}UpdateData = {
   where?: Gassma${sheetName}WhereUse;
   data: Gassma${sheetName}Use;
 }
